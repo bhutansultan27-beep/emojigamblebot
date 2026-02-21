59ed62fe5e0f64240113f3485bbd23c844f72d3d
kkkk <55012756-jameswinston423@users.noreply.replit.com>
2026-02-21 20:48:02 +0000

    WIP: local changes

--- models.py
+++ models.py
@@ -22,6 +22,7 @@ class User(db.Model):
     games_won: Mapped[int] = mapped_column(Integer, default=0)
     win_streak: Mapped[int] = mapped_column(Integer, default=0)
     best_win_streak: Mapped[int] = mapped_column(Integer, default=0)
+    total_won: Mapped[float] = mapped_column(Float, default=0.0)
     wagered_since_last_withdrawal: Mapped[float] = mapped_column(Float, default=0.0)
     first_wager_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
     referral_code: Mapped[str] = mapped_column(String, nullable=True)
